48312b37b96bfd48a65b76a3882fc12821314f00
qingqingqingli <kadlqbetty@gmail.com>
2021-02-18 11:29:44 +0100

    die after one second

---
+++ philo_one/cmake-build-debug/CMakeFiles/philo_one.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/qli/CLionProjects/philosophers/philo_one/main.c" "/home/qli/CLionProjects/philosophers/philo_one/cmake-build-debug/CMakeFiles/philo_one.dir/main.c.o"
+  "/home/qli/CLionProjects/philosophers/philo_one/srcs/check_status.c" "/home/qli/CLionProjects/philosophers/philo_one/cmake-build-debug/CMakeFiles/philo_one.dir/srcs/check_status.c.o"
+  "/home/qli/CLionProjects/philosophers/philo_one/srcs/clean_up.c" "/home/qli/CLionProjects/philosophers/philo_one/cmake-build-debug/CMakeFiles/philo_one.dir/srcs/clean_up.c.o"
+  "/home/qli/CLionProjects/philosophers/philo_one/srcs/initialise_philos.c" "/home/qli/CLionProjects/philosophers/philo_one/cmake-build-debug/CMakeFiles/philo_one.dir/srcs/initialise_philos.c.o"
+  "/home/qli/CLionProjects/philosophers/philo_one/srcs/print.c" "/home/qli/CLionProjects/philosophers/philo_one/cmake-build-debug/CMakeFiles/philo_one.dir/srcs/print.c.o"
+  "/home/qli/CLionProjects/philosophers/philo_one/srcs/process_input.c" "/home/qli/CLionProjects/philosophers/philo_one/cmake-build-debug/CMakeFiles/philo_one.dir/srcs/process_input.c.o"
+  "/home/qli/CLionProjects/philosophers/philo_one/srcs/start_action.c" "/home/qli/CLionProjects/philosophers/philo_one/cmake-build-debug/CMakeFiles/philo_one.dir/srcs/start_action.c.o"
+  "/home/qli/CLionProjects/philosophers/philo_one/srcs/support_functions.c" "/home/qli/CLionProjects/philosophers/philo_one/cmake-build-debug/CMakeFiles/philo_one.dir/srcs/support_functions.c.o"
+  "/home/qli/CLionProjects/philosophers/philo_one/srcs/time_calculation.c" "/home/qli/CLionProjects/philosophers/philo_one/cmake-build-debug/CMakeFiles/philo_one.dir/srcs/time_calculation.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ philo_one/cmake-build-debug/CMakeFiles/philo_one.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/philo_one.dir/main.c.o"
+  "CMakeFiles/philo_one.dir/srcs/check_status.c.o"
+  "CMakeFiles/philo_one.dir/srcs/clean_up.c.o"
+  "CMakeFiles/philo_one.dir/srcs/initialise_philos.c.o"
+  "CMakeFiles/philo_one.dir/srcs/print.c.o"
+  "CMakeFiles/philo_one.dir/srcs/process_input.c.o"
+  "CMakeFiles/philo_one.dir/srcs/start_action.c.o"
+  "CMakeFiles/philo_one.dir/srcs/support_functions.c.o"
+  "CMakeFiles/philo_one.dir/srcs/time_calculation.c.o"
+  "philo_one"
+  "philo_one.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/philo_one.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
