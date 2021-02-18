5c8574ee6ab690dc05edb1f6658e032b39615d0e
qingqingqingli <kadlqbetty@gmail.com>
2021-02-18 20:43:38 +0100

    remove time gap

--- philo_one/srcs/initialise_philos.c
+++ philo_one/srcs/initialise_philos.c
@@ -51,7 +51,6 @@ int		init_philo_threads(t_philosopher *philos, int num)
 	{
 		if (pthread_create(&philos[i].philo_thread, NULL, start_action, &philos[i]))
 			return (-1);
-		usleep(10);
 		if (pthread_create(&philos[i].philo_status_thread, NULL, check_status, &philos[i]))
 			return (-1);
 		i++;
