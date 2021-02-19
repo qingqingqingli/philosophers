9ae3863c747ce66c9412d576663a54391e366d0c
qingqingqingli <kadlqbetty@gmail.com>
2021-02-19 11:19:44 +0100

    test with no separate function for grab

--- philo_one/srcs/start_action.c
+++ philo_one/srcs/start_action.c
@@ -20,7 +20,22 @@ int 	lock_right_fork(t_philosopher *philo)
 	return 0;
 }
 
-int 	grab_forks(t_philosopher *philo)
+//int 	grab_forks(t_philosopher *philo)
+//{
+//	if (!philo->num % 2)
+//	{
+//		lock_left_fork(philo);
+//		lock_right_fork(philo);
+//	}
+//	else
+//	{
+//		lock_right_fork(philo);
+//		lock_left_fork(philo);
+//	}
+//	return 0;
+//}
+
+int		philo_eat(t_philosopher *philo)
 {
 	if (!philo->num % 2)
 	{
@@ -32,11 +47,6 @@ int 	grab_forks(t_philosopher *philo)
 		lock_right_fork(philo);
 		lock_left_fork(philo);
 	}
-	return 0;
-}
-
-int		philo_eat(t_philosopher *philo)
-{
 	pthread_mutex_lock(&philo->status_mutex);
 	print_prompt(philo, "is eating");
 	gettimeofday(&philo->last_eat_time, NULL);
@@ -70,7 +80,7 @@ void*	start_action(void *arg)
 	philo = arg;
 	while (philo->setup->life_status)
 	{
-		if (grab_forks(philo) || philo_eat(philo) || philo_sleep(philo) || philo_think(philo))
+		if (philo_eat(philo) || philo_sleep(philo) || philo_think(philo))
 			return NULL;
 	}
 	return NULL;
