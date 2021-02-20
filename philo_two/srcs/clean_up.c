cf58245a6b06891381c4fef645a4563869edcde7
qingqingqingli <kadlqbetty@gmail.com>
2021-02-20 20:34:15 +0100

    philo_two - update cleanup function

--- philo_two/srcs/clean_up.c
+++ philo_two/srcs/clean_up.c
@@ -4,7 +4,7 @@
 
 #include "../headers/clean_up.h"
 
-int 	clean_up(t_setup *setup, t_philosopher *philos, int return_value)
+int 	clean_up(t_philosopher *philos, int return_value)
 {
 	if (philos)
 		free(philos);
